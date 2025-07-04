7f5466fad8d11632da9962989590f0c096f72275
Parsa Eftekharmanesh <134914088+GavinRuff007@users.noreply.github.com>
2025-07-05 02:35:39 +0330

    Create CI/CD pipeline

--- src/database/database.go
+++ src/database/database.go
@@ -2,34 +2,17 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 // TODO : if want run in local use below function
-func InitDatabase() *sql.DB {
-
-	var err error
-	DB, err := sql.Open("mysql", "root:P@ssw0rd!2023@tcp(127.0.0.1:3306)/onlineShop?parseTime=true")
-	if err != nil {
-		panic(err)
-	}
-	if err := DB.Ping(); err != nil {
-		panic(err)
-	}
-	return DB
-}
-
 // func InitDatabase() *sql.DB {
-// 	dbUser := os.Getenv("DB_USER")
-// 	dbPass := os.Getenv("DB_PASSWORD")
-// 	dbHost := os.Getenv("DB_HOST")
-// 	dbPort := os.Getenv("DB_PORT")
-// 	dbName := os.Getenv("DB_NAME")
 
-// 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPass, dbHost, dbPort, dbName)
-
-// 	DB, err := sql.Open("mysql", dsn)
+// 	var err error
+// 	DB, err := sql.Open("mysql", "root:P@ssw0rd!2023@tcp(127.0.0.1:3306)/onlineShop?parseTime=true")
 // 	if err != nil {
 // 		panic(err)
 // 	}
@@ -38,3 +21,22 @@ func InitDatabase() *sql.DB {
 // 	}
 // 	return DB
 // }
+
+func InitDatabase() *sql.DB {
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPass, dbHost, dbPort, dbName)
+
+	DB, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+	if err := DB.Ping(); err != nil {
+		panic(err)
+	}
+	return DB
+}
