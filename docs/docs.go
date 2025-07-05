ae6675031cf305e505ed7e96100116563f9df14b
Parsa Eftekharmanesh <134914088+GavinRuff007@users.noreply.github.com>
2025-07-05 13:53:30 +0330

    refactor Swag info

--- docs/docs.go
+++ docs/docs.go
@@ -287,7 +287,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:9004",
+	Host:             "202.133.88.175:9004",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Product API",
